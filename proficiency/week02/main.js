fb276bf6e818a876e513e1ec395702209b576c40
Pedro Bastos <pedro@pontonet.com>
2019-10-28 09:45:55 -0400

    Proficiency 2

---
+++ proficiency/week02/main.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  const buttons = document.getElementsByClassName("topic-button");
+
+  for (i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function(e) {
+      const ev = e || window.event;
+      const target = ev.target || ev.srcElement;
+      executeCode(target);
+    };
+  }
+};
+
+function executeCode(topic) {
+  const id = topic.getAttribute("data-id");
+
+  const code = document.getElementById(`topic-${id}-code`);
+  const console = document.getElementById(`topic-${id}-console`);
+
+  console.innerHTML = eval(code.innerText);
+  console.style.visibility = "visible";
+}
